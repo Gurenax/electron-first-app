8ed29fa4fbef43fefb7af1ef541d5179036f8472
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-06-04 17:24:29 +1000

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+const webpack = require('webpack')
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'App.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'] // Loads .babelrc
+      }
+    ]
+  },
+  stats: {
+    colors: true
+  },
+  devtool: 'source-map'
+}
